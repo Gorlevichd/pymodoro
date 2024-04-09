1aebad62a1dd25ca799f74cedcbadb1233f01985
Gorlevich Daniil <gorlevichd@gmail.com>
2024-04-09 16:42:14 +0300

    Added notifications, cycle breaks

--- pymodoro/__main__.py
+++ pymodoro/__main__.py
@@ -1,45 +1,45 @@
 import argparse
-from .pomodoro import pomodoro_segment
+from .pomodoro import run_loop
 import os
 import colorama
+from dataclasses import dataclass
 
-# For colored output
-colorama.init()
+############################################################# CONFIG ################################################################
+@dataclass
+class PomodoroConfig:
+    n_focus: int
+    focus_time: int
+    small_break_time: int
+    long_break_time: int
+    terminal_columns: int
+    silent_flag: bool
+
 
 # Parse CLI inputs
 parser = argparse.ArgumentParser()
-parser.add_argument("--n_cycles", nargs = "?", default = 1, type = int,
-                    help = "Number of Pomodoro cycles")
-parser.add_argument("--focus", nargs = "?", default = 25, type = int,
-                    help = "Pomodoro focus time in minutes")
-parser.add_argument("--small_break", nargs = "?", default = 5, type = int,
-                    help = "Pomodoro small break time in minutes")
 parser.add_argument("--n_focus", nargs = "?", default = 4, type = int,
                     help = "Number of focus segments per cycle")
-parser.add_argument("--long_break", nargs = "?", default = 15, type = int,
+parser.add_argument("--focus_time", nargs = "?", default = 25, type = int,
+                    help = "Pomodoro focus time in minutes")
+parser.add_argument("--small_break_time", nargs = "?", default = 5, type = int,
+                    help = "Pomodoro small break time in minutes")
+parser.add_argument("--long_break_time", nargs = "?", default = 15, type = int,
                     help = "Pomodoro long break time in minutes")
+parser.add_argument("--silent", action = "store_true")
 args = parser.parse_args()
 
 # Determine terminal size for print alignment
 terminal_columns = os.get_terminal_size().columns
 
+config = PomodoroConfig(
+    n_focus = args.n_focus, 
+    focus_time = args.focus_time, 
+    small_break_time = args.small_break_time,
+    long_break_time = args.long_break_time,
+    terminal_columns = terminal_columns,
+    silent_flag = args.silent
+)
+
+############################################################# RUN ###################################################################
 # Run pomodoro loop
-print("Pomodoro".center(terminal_columns, "_"))
-print("".center(terminal_columns, "-"))
-print(f"Starting pomodoro with {args.n_cycles} cycles".center(terminal_columns, " "))
-print(f"There is {args.n_focus} {args.focus}-minute focus segments...".center(terminal_columns, " "))
-print(f"And {args.n_focus - 1} {args.small_break}-minute small breaks...".center(terminal_columns, " "))
-print(f"Cycle finishes with {args.long_break}-minute long break".center(terminal_columns, " "))
-print("".center(terminal_columns, "-"))
-
-for cycle_i in range(args.n_cycles):
-    print(f"Starting cycle {cycle_i}".center(terminal_columns, " "))
-    print("".center(terminal_columns, "-"))
-
-    for focus_segment in range(args.n_focus):
-        pomodoro_segment("Focus", args.focus)
-
-        if (focus_segment + 1) < args.n_focus:
-            pomodoro_segment("Small Break", args.small_break)
-        else:
-            pomodoro_segment("Long Break", args.long_break)
+run_loop(config)
--- pymodoro/pomodoro.py
+++ pymodoro/pomodoro.py
@@ -3,10 +3,12 @@ import time
 from datetime import datetime, timedelta
 import argparse
 import colorama
+import subprocess
 
 
 def pomodoro_segment(name: str, minutes: int):
     print(colorama.Fore.GREEN + f"Time to start {name}")
+    subprocess.Popen(["notify-send", "Pomodoro", f"Time to start {name}"])
     input(colorama.Fore.GREEN + "Press Enter, when ready:" + colorama.Style.RESET_ALL)
 
     start = time.time()
@@ -25,4 +27,40 @@ def pomodoro_segment(name: str, minutes: int):
             elapsed = time.time() - start
             pbar.update(elapsed - pbar.n)
     print(f"{name} finished!")
-    print("")
+    print()
+
+
+def run_loop(config):
+    # Run pomodoro loop
+    print("pomodoro".center(config.terminal_columns, "_"))
+    print("".center(config.terminal_columns, "-"))
+    print(f"There is {config.n_focus} {config.focus_time}-minute focus segments...".center(config.terminal_columns, " "))
+    print(f"And {config.n_focus - 1} {config.small_break_time}-minute small breaks...".center(config.terminal_columns, " "))
+    print(f"Cycle finishes with {config.long_break_time}-minute long break".center(config.terminal_columns, " "))
+    print("".center(config.terminal_columns, "-"))
+    
+    cycle_i = 1
+    focus_counter = 0
+    try:
+        while True:
+            print(f"Starting cycle {cycle_i}".center(config.terminal_columns, " "))
+            print("".center(config.terminal_columns, "-"))
+
+            for focus_segment in range(1, config.n_focus + 1):
+                pomodoro_segment(f"Focus ({focus_segment}/{config.n_focus})", config.focus_time)
+                focus_counter += 1
+                
+                if (focus_segment) < config.n_focus:
+                    pomodoro_segment("Small Break", config.small_break_time)
+                else:
+                    pomodoro_segment("Long Break", config.long_break_time)
+            cycle_i += 1
+    except KeyboardInterrupt:
+        print()
+        print(colorama.Fore.RED + "Received Ctrl-C, exiting" + colorama.Style.RESET_ALL)
+        print()
+        print("".center(config.terminal_columns, "-"))
+        print("Current run stats")
+        print("".center(config.terminal_columns, "-"))
+        print(f"Finished cycles: {cycle_i - 1}")
+        print(f"Focus segments: {focus_counter}")
